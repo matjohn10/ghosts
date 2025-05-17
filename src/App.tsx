3a61787f3ce413d3f34a9e605288ea3b362b2809
Mathieu Johnson <mathieujohnson10@gmail.com>
2025-05-16 20:33:50 -0400

    feat: toggle light/dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { ModeToggle } from "./components/mode-toggle";
+
 function App() {
-  return <div>Hello</div>;
+  return <div className="flex-col">Hello</div>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import "./index.css";
+import { ThemeProvider } from "./components/providers/theme-provider.tsx";
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ConvexProvider client={convex}>
-      <App />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App />
+      </ThemeProvider>
     </ConvexProvider>
   </StrictMode>
 );
