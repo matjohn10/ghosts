320849fdbd601f93606e15e4a12ec5fe304c2c86
Mathieu Johnson <mathieujohnson10@gmail.com>
2025-05-16 20:18:31 -0400

    batman

---
+++ convex/schema.ts
@@ -0,0 +1,13 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  ghosts: defineTable({
+    name: v.string(),
+    lat: v.float64(),
+    long: v.float64(),
+    personality: v.string(),
+    location: v.string(), // house, building, outside, park, etc
+    encounters: v.number(),
+  }),
+});
