27ffa6490c6df2e7d1fa248deb87bfbe932ae309
Mathieu Johnson <mathieujohnson10@gmail.com>
2025-05-18 14:40:01 -0400

    get user position

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,17 +1,12 @@
-import {
-  GoogleMap,
-  useJsApiLoader,
-  OverlayViewF,
-  OverlayView,
-} from "@react-google-maps/api";
-import { useCallback, useState } from "react";
-import icon from "@/assets/vite.svg";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { useCallback, useEffect, useState } from "react";
 import { useQuery } from "convex/react";
 import { api } from "../../../convex/_generated/api";
 import { useMapForm } from "../providers/map-provider";
 import CustomMarker from "./CustomMarker";
 
 const center = { lat: 43.675694, lng: -79.376631 };
+const mapZoom = 11;
 
 function MapObject() {
   const { form } = useMapForm();
@@ -21,18 +16,39 @@ function MapObject() {
     id: "google-map-script",
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAP_API as string,
   });
+  const [pos, setPos] = useState(center);
+  //const [map, setMap] = useState<google.maps.Map | null>(null);
 
-  const [map, setMap] = useState<google.maps.Map | null>(null);
-
-  const onLoad = useCallback(function callback(map: google.maps.Map) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-    setMap(map);
+  const onLoad = useCallback(function callback(mapp: google.maps.Map) {
+    mapp.setZoom(mapZoom);
+    //setMap(mapp);
   }, []);
 
   const onUnmount = useCallback(function callback(map: google.maps.Map) {
-    setMap(null);
+    //setMap(null);
+  }, []);
+
+  function success(position: GeolocationPosition) {
+    console.log(
+      "Latitude: " + position.coords.latitude,
+      "Longitude: " + position.coords.longitude
+    );
+    setPos({ lat: position.coords.latitude, lng: position.coords.latitude });
+  }
+
+  function error() {
+    console.log("No position");
+    //alert("Sorry, no position available.");
+  }
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((p) => success(p), error, {
+        timeout: 5000,
+      });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
   }, []);
 
   if (loadError) return <div>Error loading maps</div>;
@@ -62,10 +78,10 @@ function MapObject() {
     <div className="w-full h-full rounded-xl flex items-center justify-center overflow-hidden">
       <GoogleMap
         mapContainerStyle={{ width: "100%", height: "100%" }}
-        center={center}
+        center={pos}
         onLoad={onLoad}
         onUnmount={onUnmount}
-        zoom={1}
+        zoom={mapZoom}
         onDblClick={(e) => {
           if (!form) return;
           form.setValue("lat", e.latLng?.lat() ?? 0);
